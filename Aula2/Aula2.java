b13dff363094ed2b1652d330ddce55a1b11e50ad
Igor Silva Pivoto <studiosstarchip@gmail.com>
2026-02-10 14:27:36 -0300

    Public aula2

---
+++ Aula2/Aula2.java
@@ -0,0 +1,35 @@
+
+public class Aula2 {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá, mundo!");
+
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        
+        System.out.println("A soma de x + y é: " + z);
+    }
+
+    // int num1;
+    // int num2;
+
+    // int soma(int num1, int num2){
+    //     return num1 + num2;
+    // }
+
+    // public static void main(String[] args) {
+    //     Aula2 m = new Aula2();
+    //     try (Scanner scanner = new Scanner(System.in)) {
+    //         System.out.print("Digite o primeiro número: ");
+    //         m.num1 = scanner.nextInt();
+
+    //         System.out.print("Digite o segundo número: ");
+    //         m.num2 = scanner.nextInt();
+    //     }
+
+    //     int resultado = m.soma(m.num1, m.num2);
+    //     System.out.println("A soma dos números é: " + resultado);
+    // }
+}
